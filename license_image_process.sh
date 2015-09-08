5d40b3734850139f1a44dea46440d24d87f9b9a8
Winston Featherly-Bean <winstonfb@gmail.com>
2015-09-08 01:10:30 -0700

    Fixed parameters in image processing script.

--- license_image_process.sh
+++ license_image_process.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mogrify -colorspace gray -filter cubic -resize 300% -unsharp 7x3+0.0 *.jpg
+mogrify -colorspace gray -filter cubic -resize 300% -unsharp 7x3+3+0.0 *.jpg
 list="ls *.jpg"
 for i in $list; do tesseract $i ${i%.jpg}; done
